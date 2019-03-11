f11222fcf3f139da8445a6f44f2dbac1d25afb6e
Cory Aitchison <corytaitchison@gmail.com>
2019-03-11 20:30:35 +1100

    Update question 1

--- question1.r
+++ question1.r
@@ -1,12 +1,5 @@
 
-# Q1
-# "Is there a correlation between majors that promote higher paying jobs and the associated unemployment rates?" 
-# Scatter plot (LOB)
+library(ggplot2)
 
-# Q2
-# "Which college degrees are most relevant to the subsequent job (based on number of college jobs)?" 
-
-# Q3 *Based on previous rates
-# "What are the comparative unemployment rates between Engineering and Liberal Arts graduates?" 
-# Unemployment rate between major categories (Simpson's Paradox) potential
-# Get top 10 from each Humanities and Liberal Arts + Engineering 
+p = ggplot(gradData[gradData$Major_category == "Humanities & Liberal Arts",], aes(x=Median, y=Unemployment_rate))
+p + geom_point(aes(color = factor(Major_category)))
