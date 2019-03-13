c5421d9f3a1e9c084a0d50d160115afad09e30cf
Cory Aitchison <corytaitchison@gmail.com>
2019-03-13 20:08:26 +1100

    Add initial graphs

--- question1.r
+++ question1.r
@@ -1,5 +1,22 @@
 
 library(ggplot2)
 
-p = ggplot(gradData[gradData$Major_category == "Humanities & Liberal Arts",], aes(x=Median, y=Unemployment_rate))
-p + geom_point(aes(color = factor(Major_category)))
+# Gender $ vs Income
+# Removed outlier value <$1e5
+ggplot(gradData[gradData$Median<100000,], aes(x=ShareWomen*100, y=Median/1000)) + 
+  geom_point(aes(color = factor(Major_category))) + xlab("% Women") + ylab("Median Income ($1000)") + 
+  labs(color = "Major Category", title="Median Income vs Percentage of Women in Major") + 
+  stat_smooth(method = "lm", col = "black")
+
+# Major vs Income
+ggplot(data = gradData, aes(Median)) + geom_histogram(aes(fill = factor(gradData$Major_category)), bins = 10) +
+  labs(fill = "Major Category", title="Distribution of Median Income per Major Category")
+
+# Major vs College Jobs %
+percentCollege = gradData$College_jobs/(gradData$College_jobs+gradData$Non_college_jobs+gradData$Low_wage_jobs)
+ggplot(gradData, aes(x=factor(Major_category), y=percentCollege)) + 
+  geom_boxplot(aes(fill = factor(Major_category))) +
+  theme(axis.text.x = element_blank(), axis.title.x = element_blank(), axis.ticks.x = element_blank()) +
+  labs(fill = "Major Category", title="Percent College Jobs per Major Category") + 
+  ylab("Percentage of Jobs as College Jobs")
+
