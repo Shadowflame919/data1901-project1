633cb95ed6d8e8bb4c9b7217c96f627ea969ae3e
EnBr <30426039+EnBr55@users.noreply.github.com>
2019-03-22 00:28:15 +1100

    Add nice plots and remove junk

--- question3.r
+++ question3.r
@@ -3,35 +3,34 @@ library(ggplot2)
 library(plotly)
 
 # Exclude Food Science, as it has missing data.
+# total_grads = sum(gradTotalData$Total)
+# Order our data by the total number of graduates
 gradTotalData = gradData[!(gradData$Major=="FOOD SCIENCE") & !(gradData$Major=="MILITARY TECHNOLOGIES") ,]
 
-# Order our data by the total number of graduates
 gradTotalData = gradTotalData[order(gradTotalData$Total),]
-
-total_grads = sum(gradTotalData$Total)
 
 bottom_ten_by_total = head(gradTotalData, 10)
 top_ten_by_total = tail(gradTotalData, 10)
 
 gradTotal_topbottom = rbind(top_ten_by_total, bottom_ten_by_total)
 
-plotAllMedianVsPop = ggplot(gradTotalData, aes(x=Median, y=Total))
-plotAllMedianVsPop + geom_point()
-
-plotTopMedianVsPop = ggplot(gradTotal_topbottom, aes(x=Median, y=Total, color=Major))
-plotTopMedianVsPop + geom_point()
-
-plotAllEmployedVsPop = ggplot(gradTotalData, aes(x=Employed/Total, y=Total))
-plotAllEmployedVsPop + geom_point()
-
-plotTopEmployedVsPop = ggplot(gradTotal_topbottom, aes(x=Employed/Total, y=Total, color=Major))
-plotTopEmployedVsPop + geom_point()
 
 
 
+ggplotly(ggplot(gradTotal_topbottom, aes(x=Median, fill=Major, y=Total)) + geom_point() + theme(legend.position = "none") +
+           xlab("Median Income") + ylab("Popularity") + labs(title="Popularity vs Median Income"))
 
 
+ggplotly(ggplot(gradTotalData, aes(x=Employed/Total, fill=Major_category)) + geom_histogram(bins = 15) + theme(legend.position = "none") +
+           xlab("Employment Rate") + ylab("Density") + labs(title="Popularity vs Median Income"))
 
-#PROBLEMS:
-#"Graduates" isn't necessarily a metric of popularity. 
-# i.e. 'Psychology' could be considered an easier course and therefore recieve more graduates
+
+ggplotly(ggplot(gradTotalData, aes(x=Median, fill=Major_category)) + geom_histogram(bins = 15) + theme(legend.position = "none") +
+           xlab("Median Income") + ylab("Density") + labs(title="Popularity vs Median Income"))
+
+
+employmentMean = mean((gradTotalData$Employed/gradTotalData$Total))
+employmentSd = sd((gradTotalData$Employed/gradTotalData$Total))
+# Get all employment rates of engineering, then sort them in increasing order, and take the first element to get the lowest.
+lowestEmployment = sort((gradTotalData$Employed/gradTotalData$Total)[gradTotalData$Major_category=="Engineering"], decreasing=FALSE)[1]
+distance = ((employmentMean - lowestEmployment)/employmentSd)
